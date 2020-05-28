3af08dbd891f40e2a5728054ad221911371e7c24
Prakriti <rajbhandariprakriti.7@gmail.com>
2020-05-28 21:45:35 +1000

    member.js file added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -30,6 +30,38 @@ module.exports = function(app) {
       });
   });
 
+<<<<<<< Updated upstream
+=======
+  // Route for updating user
+  app.put("/api/member/:id", (req, res) => {
+    // log body of request when passed from front
+    console.log(req.body);
+    db.User.update(
+      {
+        first_name: req.body["first-name"],
+        last_name: req.body["last-name"],
+        age: req.body.age,
+        sex: req.body.sex,
+        mobile: req.body.mobile,
+        height: req.body.height,
+        weight: req.body.weight,
+        goal_weight: req.body["target-weight"],
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then(() => {
+        res.redirect(302, "/api/user_data");
+      })
+      .catch((err) => {
+        res.status(401).json(err);
+      });
+  });
+
+>>>>>>> Stashed changes
   // Route for logging user out
   app.get("/logout", (req, res) => {
     req.logout();
@@ -47,6 +79,17 @@ module.exports = function(app) {
       res.json({
         email: req.user.email,
         id: req.user.id,
+<<<<<<< Updated upstream
+=======
+        "first-name": req.user.first_name,
+        "last-name": req.user.last_name,
+        age: req.user.age,
+        sex: req.user.sex,
+        mobile: req.user.mobile,
+        height: req.user.height,
+        weight: req.user.weight,
+        "target-weight": req.user.goal_weight,
+>>>>>>> Stashed changes
       });
     }
   });
