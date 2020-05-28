a6e6cd9134a76627436f0df350609bd87d23998d
Solmarnov <solmarnov@gmail.com>
2020-05-28 19:12:23 +1000

    Added route for updating User

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -30,6 +30,32 @@ module.exports = function(app) {
       });
   });
 
+  // Route for updating user 
+  app.put("/api/member/:id", (req, res) => {
+    // log body of request when passed from front
+    console.log(req.body);
+    db.User.update({
+      first_name: req.body["first-name"],
+      last_name: req.body["last-name"],
+      age: req.body.age,
+      sex: req.body.sex,
+      mobile: req.body.mobile,
+      height: req.body.height,
+      weight: req.body.weight,
+      goal_weight: req.body["target-weight"]
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(() => {
+      res.redirect(302, "/api/user_data")
+    })
+    .catch(err => {
+      res.status(401).json(err);
+    });
+  });
+
   // Route for logging user out
   app.get("/logout", (req, res) => {
     req.logout();
@@ -46,7 +72,15 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
+        "first-name": req.user.first_name,
+        "last-name": req.user.last_name,
+        age: req.user.age,
+        sex: req.user.sex,
+        mobile: req.user.mobile,
+        height: req.user.height,
+        weight: req.user.weight,
+        "target-weight": req.user.goal_weight
       });
     }
   });
