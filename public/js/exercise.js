6f2f97bd7ca500ed8da31800496a12370c9f40e8
Prakriti <rajbhandariprakriti.7@gmail.com>
2020-06-02 21:03:29 +1000

    update exercise.js

--- public/js/exercise.js
+++ public/js/exercise.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
-    $.get("/api/user_data").then(data => {
-        $(".blog-container").text(data.email);
+    $.get("/api/exercises_data").then(data => {
+        $(".blog-container").text(data.exercise_name);
     });
 });
